3dd6f32562201ccebea5e8563bc7d679b51fd7f9
saritadipa34 <saritadipa34@gmail.com>
2025-04-22 08:44:23 +0530

    Added counter div for showing length

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -3,6 +3,11 @@ import MovieCard from "./MovieCard";
 
 const WatchList=({watchList,watchedList,setWatchedList,setWatchList})=>{
 
+    const removeWishListed=(movie)=>{
+        const filtered=watchList.filter((item)=>item.imdbID !== movie.imdbID);
+        setWatchList(filtered);
+        console.log(filtered)
+    }
     
     const addWatched=(movie)=>{
 if(!watchedList.some((item)=>item.imdbID === movie.imdbID)){
@@ -10,19 +15,21 @@ if(!watchedList.some((item)=>item.imdbID === movie.imdbID)){
 setWatchedList(watchedMovie);
 const filtered=watchList.filter((item)=>item.imdbID !== movie.imdbID);
 setWatchList(filtered);
-localStorage.setItem('wishList',JSON.stringify(filtered));
 }
 
 console.log("watched");
 }
 
     return(
-        <div className="px-[190px] w-full h-screen">
+        <div className="w-full h-screen px-[190px]">
+            <div className="flex items-center justify-between">
             <h1 className="font-bold text-2xl my-4">My WatchList</h1>
+            <div className=" bg-green-600 mr-4 h-[25px] min-w-[50px] my-4 text-sm rounded-lg flex justify-center items-center font-bold text-white">{watchList.length}</div>
+            </div>
             <div className="h-full w-full flex flex-wrap gap-5 "> 
 
                 { watchList.length > 0 &&  watchList.map((list)=>{
-  return  <MovieCard addWatched={()=>addWatched(list)}  movie={list} key={list.imdbID}/>
+  return  <MovieCard addWatched={()=>addWatched(list)} removeWatched={()=>removeWishListed(list)}  movie={list} key={list.imdbID}/>
                 })
                 }
           
--- src/components/Watched.jsx
+++ src/components/Watched.jsx
@@ -4,13 +4,15 @@ const Watched=({watchedList,setWatchedList})=>{
 
     const removeWatched=(movie)=>{
         const removed=watchedList.filter((item)=>item.imdbID !== movie.imdbID)
-            console.log("removeWatched",removed);
             setWatchedList(removed);
         }
 
     return(
         <div className="px-[190px] w-full h-screen ">
+            <div className="flex items-center justify-between">
             <h1 className="font-bold text-2xl my-4">My WatchedList</h1>
+            <div className=" bg-green-600 mr-4 h-[25px] min-w-[50px] my-4 text-sm rounded-lg flex justify-center items-center font-bold text-white">{watchedList.length}</div>
+            </div>
               <div className="h-full w-full flex flex-wrap gap-5 "> 
 {
     watchedList.length> 0 && watchedList.map((film,index)=>{
